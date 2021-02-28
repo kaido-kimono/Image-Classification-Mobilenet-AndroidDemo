801a58ffa6e4acd7394d0867593398cc56577ad7
Showee Mayeze <showeemayeze@outlook.fr>
2021-02-28 19:05:01 +0200

    Adding Localisation

--- app/src/main/java/com/example/imageclassificationdemo/ui/addplantation/AddPlantationFragment.java
+++ app/src/main/java/com/example/imageclassificationdemo/ui/addplantation/AddPlantationFragment.java
@@ -1,5 +1,7 @@
 package com.example.imageclassificationdemo.ui.addplantation;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -19,6 +21,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.schibstedspain.leku.LocationPickerActivity;
 
 import java.util.UUID;
 
@@ -35,31 +38,55 @@ public class AddPlantationFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        MaterialButton button = view.findViewById(R.id.btnAdd);
-        final TextInputEditText edName = view.findViewById(R.id.edName);
-        final TextInputEditText edSuperficie = view.findViewById(R.id.edSupercifie);
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            if (requestCode == 12) {
+                double latitude = data.getDoubleExtra("latitude", 0.0);
+                double longitude = data.getDoubleExtra("longitude", 0.0);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                final TextInputEditText edName = requireView().findViewById(R.id.edName);
+                final TextInputEditText edSuperficie = requireView().findViewById(R.id.edSupercifie);
+
+
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 String plantationUid = UUID.randomUUID().toString();
                 DocumentReference docRef = FirebaseFirestore.getInstance()
                         .document("utilisateurs/" + user.getUid() + "/plantations/" + plantationUid);
 
-                Plantation plantation = new Plantation(
-                    docRef.getId(),
-                    edName.getText().toString(),
-                    Double.parseDouble(edSuperficie.getText().toString()),
-                    0
-                );
+                Plantation plantation = new Plantation();
+                plantation.setUid(docRef.getId());
+                plantation.setSuperficie(Double.parseDouble(edSuperficie.getText().toString()));
+                plantation.setNom(edName.getText().toString());
+                plantation.setLatitude(latitude);
+                plantation.setLongitude(longitude);
 
                 docRef.set(plantation);
-                NavController navController = Navigation.findNavController(v);
+                NavController navController = Navigation.findNavController(requireView());
                 navController.navigateUp();
             }
+        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        MaterialButton button = view.findViewById(R.id.btnAdd);
+
+        button.setOnClickListener(v -> {
+            Intent locationPickerIntent = new LocationPickerActivity.Builder()
+                .withDefaultLocaleSearchZone()
+                .withStreetHidden()
+                .withLegacyLayout()
+                .withCityHidden()
+                .withZipCodeHidden()
+                .withSatelliteViewHidden()
+                .withGoogleTimeZoneEnabled()
+                .withVoiceSearchHidden()
+                .withUnnamedRoadHidden()
+                .build(getContext());
+
+            startActivityForResult(locationPickerIntent, 12);
         });
     }
 }
--- app/src/main/java/models/Plantation.java
+++ app/src/main/java/models/Plantation.java
@@ -7,20 +7,18 @@ public class Plantation implements Serializable {
     private String uid;
     private String nom;
     private double superficie;
-    private double localisation;
+    private double longitude;
+    private double latitude;
 
-    public Plantation(String uid, String nom, double superficie, double localisation) {
+    public Plantation(String uid, String nom, double superficie, double longitude, double latitude) {
         this.uid = uid;
         this.nom = nom;
         this.superficie = superficie;
-        this.localisation = localisation;
+        this.longitude = longitude;
+        this.latitude = latitude;
     }
 
     public Plantation() {
-    }
-
-    public double getSuperficie() {
-        return  this.superficie;
     }
 
     public String getUid() {
@@ -31,23 +29,35 @@ public class Plantation implements Serializable {
         this.uid = uid;
     }
 
-    public void setSuperficie(double superficie) {
-        this.superficie = superficie;
-    }
-
-    public double getLocalisation() {
-        return localisation;
-    }
-
-    public void setLocalisation(double localisation) {
-        this.localisation = localisation;
-    }
-
     public String getNom() {
         return nom;
     }
 
     public void setNom(String nom) {
         this.nom = nom;
+    }
+
+    public double getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
     }
 }
